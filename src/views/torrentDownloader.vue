52adcc69d866a339d20490b6d053f9d8cd0984d8
Jordi Hoven <35233121+jordihoven@users.noreply.github.com>
2024-09-21 10:20:43 +0000

    prettify dropzone & refactor to composition api

--- src/views/torrentDownloader.vue
+++ src/views/torrentDownloader.vue
@@ -1,86 +1,157 @@
 <template>
-  <div class="torrent-downloader">
-    <p>Select a .torrent File</p>
-
-    <!-- File input for selecting the torrent file -->
-    <input type="file" @change="handleFileUpload" accept=".torrent" />
-
-    <p v-if="file">Selected file: {{ file.name }}</p>
+  <div class="app-container">
+    <div class="torrent-downloader">
+      <div ref="dropZoneRef" class="dropzone" :class="{ dragover: isOverDropZone }" @click="openFileExplorer">
+        <p>Drag and drop .torrent files here.</p>
+        <input type="file" :accept="acceptedTypesString" ref="fileInput" @change="handleFilesSelected" multiple hidden />
+      </div>
+    </div>
+    <div class="results-wrapper">
+      <div v-if="!file" class="empty-state">
+        <p class="medium">Nothing to show</p>
+        <span>Starts downloading as soon as you drop a .torrent file... ✨</span>
+      </div>
+      <div v-if="file">
+        <p>Filename: {{ file.name }}</p>
+        <p>Progress: {{ progress.toFixed(2) }} %</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// imports...
-// TODO: check why this import causes errors, temp fix is loading the cdn in index.html (which i hate...)
-// import WebTorrent from 'webtorrent'
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useDropZone } from '@vueuse/core'
 
-export default {
-  data() {
-    return {
-      file: null,
-      progress: 0
-    }
-  },
-  created() {
-    this.client = new window.WebTorrent() // initialize client once the component is created...
-  },
-  methods: {
-    // Handle the file upload event
-    handleFileUpload(event) {
-      const selectedFile = event.target.files[0] // Get the selected file
-      if (selectedFile) {
-        this.file = selectedFile
-        console.log('File selected:', this.file) // Log the file to check
-        this.loadTorrent(this.file)
-      } else {
-        alert('Please select a valid .torrent file')
-      }
-    },
-    loadTorrent(torrentFile) {
-      this.client.add(torrentFile, (torrent) => {
-        console.log('Torrent info:', torrent)
+const dropZoneRef = ref(null)
+const fileInput = ref(null)
+const file = ref(null)
+const progress = ref(0)
+const acceptedTypesString = '.torrent'
+const { isOverDropZone } = useDropZone(dropZoneRef, onDrop)
 
-        torrent.on('download', (bytes) => {
-          console.log('just downloaded: ' + bytes)
-          console.log('progress: ' + torrent.progress)
-          this.downloadProgress(torrent)
-        })
+let client
 
-        torrent.on('done', () => {
-          console.log('Download finished!')
-          this.downloadFiles(torrent.files)
-        })
-      })
-    },
-    downloadProgress(torrent) {
-      const downloaded = torrent.downloaded
-      const total = torrent.length
-      this.progress = (downloaded / total) * 100
-    },
-    downloadFiles(files) {
-      files.forEach((file) => {
-        file.getBuffer((err, buffer) => {
-          if (err) throw err
+function onDrop(files) {
+  handleFileUpload(files)
+}
 
-          // Create a Blob from the buffer
-          const blob = new Blob([buffer], { type: file.type })
-          const url = window.URL.createObjectURL(blob)
+const openFileExplorer = () => {
+  fileInput.value.click()
+}
 
-          // Create an anchor element to trigger download
-          const a = document.createElement('a')
-          a.href = url
-          a.download = file.name
-          document.body.appendChild(a)
-          a.click()
-          a.remove()
+const handleFilesSelected = (event) => {
+  const files = event.target.files
+  handleFileUpload(files)
+}
 
-          // Clean up
-          window.URL.revokeObjectURL(url)
-        })
-      })
-    }
+const handleFileUpload = (files) => {
+  const selectedFile = files[0]
+  if (selectedFile) {
+    file.value = selectedFile
+    console.log('File selected:', file.value)
+    loadTorrent(file.value)
+  } else {
+    alert('Please select a valid .torrent file')
   }
 }
+
+const loadTorrent = (torrentFile) => {
+  client.add(torrentFile, (torrent) => {
+    console.log('Torrent info:', torrent)
+
+    torrent.on('download', (bytes) => {
+      console.log('just downloaded: ' + bytes)
+      downloadProgress(torrent)
+    })
+
+    torrent.on('done', () => {
+      console.log('Download finished!')
+      downloadFiles(torrent.files)
+    })
+  })
+}
+
+const downloadProgress = (torrent) => {
+  const downloaded = torrent.downloaded
+  const total = torrent.length
+  progress.value = (downloaded / total) * 100
+}
+
+const downloadFiles = (files) => {
+  files.forEach((file) => {
+    file.getBuffer((err, buffer) => {
+      if (err) throw err
+
+      const blob = new Blob([buffer], { type: file.type })
+      const url = window.URL.createObjectURL(blob)
+
+      const a = document.createElement('a')
+      a.href = url
+      a.download = file.name
+      document.body.appendChild(a)
+      a.click()
+      a.remove()
+
+      window.URL.revokeObjectURL(url)
+    })
+  })
+}
+
+onMounted(() => {
+  client = new window.WebTorrent()
+})
 </script>
 
-<style scoped lang="css"></style>
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.torrent-downloader {
+  border-bottom: 1px solid var(--stroke);
+  padding: var(--s-spacing);
+}
+
+.empty-state {
+  display: flex;
+  flex-direction: column;
+  gap: var(--xs-spacing);
+  margin: 0 auto;
+  max-width: 60rem;
+  align-items: center;
+}
+.empty-state span {
+  text-align: center;
+}
+
+.results-wrapper {
+  overflow: auto;
+  padding: var(--m-spacing) var(--s-spacing) var(--xl-spacing) var(--s-spacing);
+}
+
+.dropzone {
+  background-color: var(--bg-secondary);
+  border: 2px dashed var(--stroke);
+  border-radius: var(--radius);
+  padding: var(--s-spacing);
+  text-align: center;
+  transition: var(--transition);
+  flex: 1;
+  position: relative;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+.dropzone:hover {
+  cursor: pointer;
+  filter: brightness(95%);
+}
+
+.dropzone.dragover {
+  border-color: var(--primary);
+}
+</style>
